017e9539898203c450fd3e10a6acd2afc4be3dd4
zhangruifeng <zhangruifeng@evergrande.cn>
2018-03-05 17:16:45 +0800

    update-3-5

--- src/assets/lib/corsconfig.js
+++ src/assets/lib/corsconfig.js
@@ -2,10 +2,13 @@
 export default  () => {
     let dev_url = 'http://iot-dev-upgrade-center.egtest.cn:7777'
     let bees_url = 'http://iot-dev-upgrade-center-tice.egtest.cn:9000'
+    let hardware_url = 'http://iot-dev-upgrade-center-hardware.egtest.cn:9000'
     const curUrl = window.location.href
     if (/beeossdev/.test(curUrl) || /localhost/.test(curUrl)) {
         return dev_url
-    } else {
+    } else if (/iot-dev-upgrade-center-hardware/.test(curUrl)) {
+        return hardware_url
+    }else {
         return bees_url
     }
 
