cd1683bcf616245c912459bcf76d9c865ff35845
zhangruifeng <zhangruifeng@evergrande.cn>
2018-03-28 18:20:18 +0800

    update-zrf

--- src/assets/lib/corsconfig.js
+++ src/assets/lib/corsconfig.js
@@ -3,12 +3,15 @@ export default  () => {
     let dev_url = 'http://iot-dev-upgrade-center.egtest.cn:7777'
     let bees_url = 'http://iot-dev-upgrade-center-tice.egtest.cn:9000'
     let hardware_url = 'http://iot-dev-upgrade-center-hardware.egtest.cn:9000'
+    let pocOss_url = 'http://poc-upgrade.evergrande.me'
     const curUrl = window.location.href
     if (/beeossdev/.test(curUrl) || /localhost/.test(curUrl)) {
         return dev_url
     } else if (/hardtest-oss.evergrande.me/.test(curUrl)) {
         return hardware_url
-    }else {
+    }else if(/poc-oss.evergrande.me/.test(curUrl)) {
+        return pocOss_url
+    } else {
         return bees_url
     }
 
