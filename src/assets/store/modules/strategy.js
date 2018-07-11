4be95b2c3ee6ec8b4d432db42ecdf2ec749d9251
zhangxin <zhangxin2@evergrande.cn>
2018-07-11 15:15:14 +0800

    添加策略相关操作

--- src/assets/store/modules/strategy.js
+++ src/assets/store/modules/strategy.js
@@ -43,7 +43,79 @@ const actions = {
                 console.log(error)
             })
         })
-    }
+    },
+    StrategyUuidList({commit, state}, params) {
+        return new Promise((resolve, reject) => {
+            axios.post(STRATEGY_API.StrategyUuidList, params).then((response) => {
+                resolve(response.data)
+            }).catch((error) => {
+                console.log(error)
+            })
+        })
+    },
+    StrategyUuidAdd({commit, state}, params) {
+        return new Promise((resolve, reject) => {
+            axios.post(STRATEGY_API.StrategyUuidAdd, params).then((response) => {
+                resolve(response.data)
+            }).catch((error) => {
+                console.log(error)
+            })
+        })
+    },
+    StrategyUuidEdit({commit, state}, params) {
+        return new Promise((resolve, reject) => {
+            axios.post(STRATEGY_API.StrategyUuidEdit, params).then((response) => {
+                resolve(response.data)
+            }).catch((error) => {
+                console.log(error)
+            })
+        })
+    },
+    StrategyUuidDel({commit, state}, params) {
+        return new Promise((resolve, reject) => {
+            axios.post(STRATEGY_API.StrategyUuidDel, params).then((response) => {
+                resolve(response.data)
+            }).catch((error) => {
+                console.log(error)
+            })
+        })
+    },
+    StrategyConfiglists({commit, state}, params) {
+        return new Promise((resolve, reject) => {
+            axios.post(STRATEGY_API.StrategyConfiglists, params).then((response) => {
+                resolve(response.data)
+            }).catch((error) => {
+                console.log(error)
+            })
+        })
+    },
+    StrategyConfigAdd({commit, state}, params) {
+        return new Promise((resolve, reject) => {
+            axios.post(STRATEGY_API.StrategyConfigAdd, params).then((response) => {
+                resolve(response.data)
+            }).catch((error) => {
+                console.log(error)
+            })
+        })
+    },
+    StrategyConfigEdit({commit, state}, params) {
+        return new Promise((resolve, reject) => {
+            axios.post(STRATEGY_API.StrategyConfigEdit, params).then((response) => {
+                resolve(response.data)
+            }).catch((error) => {
+                console.log(error)
+            })
+        })
+    },
+    StrategyConfigDel({commit, state}, params) {
+        return new Promise((resolve, reject) => {
+            axios.post(STRATEGY_API.StrategyConfigDel, params).then((response) => {
+                resolve(response.data)
+            }).catch((error) => {
+                console.log(error)
+            })
+        })
+    },
 }
 export default {
     actions
