956b8a3d558e303e1855070bc73e98d15ec6090b
rich <sheruixu@evergrande.com>
2019-04-25 17:04:46 +0800

    格式化

--- src/pages/routerManager/routerManager.vue
+++ src/pages/routerManager/routerManager.vue
@@ -3,20 +3,26 @@
     <!--顶部tab-->
     <div style="position: relative">
       <el-row :gutter="24">
-        <el-col :span="3" 
-style="padding-right: 0; ">
-          <el-input v-model="listParams.router_id" 
-placeholder="router_id"/>
+        <el-col
+          :span="3"
+          style="padding-right: 0; ">
+          <el-input
+            v-model="listParams.router_id"
+            placeholder="router_id"/>
         </el-col>
-        <el-col :span="3" 
-style="padding-right: 0; ">
-          <el-input v-model="listParams.device_uuid" 
-placeholder="device_uuid"/>
+        <el-col
+          :span="3"
+          style="padding-right: 0; ">
+          <el-input
+            v-model="listParams.device_uuid"
+            placeholder="device_uuid"/>
         </el-col>
-        <el-col :span="2" 
-style="padding-right: 0; ">
-          <el-button type="primary" 
-@click="getRouterList()">&nbsp;&nbsp;查询&nbsp;&nbsp;</el-button>
+        <el-col
+          :span="2"
+          style="padding-right: 0; ">
+          <el-button
+            type="primary"
+            @click="getRouterList()">&nbsp;&nbsp;查询&nbsp;&nbsp;</el-button>
         </el-col>
       </el-row>
       <div style="position: absolute; top:0; right: 0">
@@ -32,83 +38,118 @@ style="padding-right: 0; ">
     </div>
     <!--列表-->
     <div style="margin-top: 20px">
-      <el-table :data="routerList.tableData" 
-style="width: 100%; border-top:0 none">
-        <el-table-column v-for="item in routerList.tableColumn" 
-:key="item.prop" :prop="item.prop" :label="item.label" :width="'auto'">
+      <el-table
+        :data="routerList.tableData"
+        style="width: 100%; border-top:0 none">
+        <el-table-column
+          v-for="item in routerList.tableColumn"
+          :key="item.prop"
+          :prop="item.prop"
+          :label="item.label"
+          :width="'auto'">
           <template slot-scope="scope">
             <div v-if="item.prop == 'device_state'">{{ getStatusText(scope.row.device_state) }}</div>
             <div v-else>{{ scope.row[item.prop] }}</div>
           </template>
         </el-table-column>
-        <el-table-column width="180" 
-label="操作">
+        <el-table-column
+          width="180"
+          label="操作">
           <template slot-scope="scope">
-            <el-button type="text" 
-size="small" @click="openEditLayer(scope.row)">编辑</el-button>
-            <el-button type="text" 
-size="small" @click="delRouter(scope.row)">删除</el-button>
+            <el-button
+              type="text"
+              size="small"
+              @click="openEditLayer(scope.row)">编辑</el-button>
+            <el-button
+              type="text"
+              size="small"
+              @click="delRouter(scope.row)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
       <div class="page-line">
-        <el-pagination small 
-:total="totalItem" layout="prev, pager, next" :page-size="10" @current-change="pageChange" :current-page.sync="currentPage"/>
+        <el-pagination
+          :total="totalItem"
+          :page-size="10"
+          :current-page.sync="currentPage"
+          small
+          layout="prev, pager, next"
+          @current-change="pageChange"/>
       </div>
     </div>
     <!--添加删除-->
-    <el-dialog :title="addEditFlag?'添加路由':'编辑路由'" 
-:visible.sync="addEditLayer">
+    <el-dialog
+      :title="addEditFlag?'添加路由':'编辑路由'"
+      :visible.sync="addEditLayer">
       <div class="edit_form">
-        <el-form :model="AddEditForm" 
-ref="AddEditForm" :rules="rulesAddEdit" label-width="100px">
+        <el-form
+          ref="AddEditForm"
+          :model="AddEditForm"
+          :rules="rulesAddEdit"
+          label-width="100px">
           <el-row>
             <el-col :span="12">
-              <el-form-item label="设备名称" 
-prop="device_name">
-                <el-input v-model="AddEditForm.device_name" 
-placeholder=""/>
+              <el-form-item
+                label="设备名称"
+                prop="device_name">
+                <el-input
+                  v-model="AddEditForm.device_name"
+                  placeholder=""/>
               </el-form-item>
             </el-col>
             <el-col :span="12">
-              <el-form-item label="UUID" 
-prop="device_uuid">
-                <el-input v-model="AddEditForm.device_uuid" 
-placeholder=""/>
+              <el-form-item
+                label="UUID"
+                prop="device_uuid">
+                <el-input
+                  v-model="AddEditForm.device_uuid"
+                  placeholder=""/>
               </el-form-item>
             </el-col>
           </el-row>
           <el-row>
             <el-col :span="12">
-              <el-form-item label="设备验证KEY" 
-prop="device_auth_key">
-                <el-input v-model="AddEditForm.device_auth_key" 
-placeholder=""/>
+              <el-form-item
+                label="设备验证KEY"
+                prop="device_auth_key">
+                <el-input
+                  v-model="AddEditForm.device_auth_key"
+                  placeholder=""/>
               </el-form-item>
             </el-col>
             <el-col :span="12">
-              <el-form-item label="设备编号" 
-prop="device_sn">
-                <el-input v-model="AddEditForm.device_sn" 
-placeholder=""/>
+              <el-form-item
+                label="设备编号"
+                prop="device_sn">
+                <el-input
+                  v-model="AddEditForm.device_sn"
+                  placeholder=""/>
               </el-form-item>
             </el-col>
           </el-row>
           <el-row>
             <el-col :span="12">
-              <el-form-item label="设备MAC" 
-prop="device_mac">
-                <el-input v-model="AddEditForm.device_mac" 
-placeholder=""/>
+              <el-form-item
+                label="设备MAC"
+                prop="device_mac">
+                <el-input
+                  v-model="AddEditForm.device_mac"
+                  placeholder=""/>
               </el-form-item>
             </el-col>
             <el-col :span="12">
-              <el-form-item label="设备状态" 
-prop="device_state">
-                <el-select v-model="AddEditForm.device_state" 
-placeholder="请选择">
-                  <el-option :value="1" label="正常"/>
-                  <el-option :value="0" label="禁用"/>
+              <el-form-item
+                label="设备状态"
+                prop="device_state">
+                <el-select
+                  v-model="AddEditForm.device_state"
+                  placeholder="请选择">
+                  <el-option
+                    :value="1"
+                    label="正常"/>
+                  <el-option
+                    :value="0"
+                    label="禁用"/>
                 </el-select>
               </el-form-item>
             </el-col>
@@ -116,16 +157,18 @@ placeholder="请选择">
           <el-row>
             <el-col :span="12">
               <el-form-item label="设备参数">
-                <el-input v-model="AddEditForm.device_params" 
-placeholder=""/>
+                <el-input
+                  v-model="AddEditForm.device_params"
+                  placeholder=""/>
               </el-form-item>
             </el-col>
           </el-row>
         </el-form>
       </div>
       <div style="text-align: right; margin: 0">
-        <el-button type="primary" 
-@click="addEditConfirm('AddEditForm')">确定</el-button>
+        <el-button
+          type="primary"
+          @click="addEditConfirm('AddEditForm')">确定</el-button>
         <el-button @click="addEditLayer=false">取消</el-button>
       </div>
     </el-dialog>
@@ -143,20 +186,20 @@ export default {
     filters: {
         typeToString(x) {
             if (x * 1 === 1) {
-                return "APP";
+                return "APP"
             } else if (x * 1 === 2) {
-                return "路由器";
+                return "路由器"
             } else if (x * 1 === 3) {
-                return "子设备";
+                return "子设备"
             } else {
-                return "未知";
+                return "未知"
             }
         },
         forceToString(x) {
-            return x ? "是" : "否";
+            return x ? "是" : "否"
         },
         statusToString(x) {
-            return x ? "启用" : "停用";
+            return x ? "启用" : "停用"
         }
     },
     data() {
@@ -190,7 +233,7 @@ export default {
                 device_mac: [{ required: true, message: "请输入设备MAC" }],
                 device_state: [{ required: true, message: "请选择设备状态" }]
             }
-        };
+        }
     },
     watch: {},
     mounted() {
@@ -274,22 +317,22 @@ export default {
             switch (type) {
                 case 1:
                     text = "工厂测试"
-                    break;
+                    break
                 case 2:
                     text = "库存"
-                    break;
+                    break
                 case 3:
                     text = "销售出"
-                    break;
+                    break
                 case 5:
                     text = "返修"
-                    break;
+                    break
                 case 6:
                     text = "失效"
-                    break;
+                    break
                 default:
                     text = "未知"
-                    break;
+                    break
             }
             return text
         },
